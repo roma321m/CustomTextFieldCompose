0997eb9b1fd57113c4b318d9ba998bd37b3f8efc
Roman Michailov <roman.michailov@intel.com>
2023-04-21 15:42:20 +0300

    Added OutlinedPasswordTextField

--- app/src/main/java/com/roman/customtextfieldcompose/MainActivity.kt
+++ app/src/main/java/com/roman/customtextfieldcompose/MainActivity.kt
@@ -3,10 +3,18 @@ package com.roman.customtextfieldcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.roman.custom_text_field_compose.OutlinedPasswordTextField
 import com.roman.customtextfieldcompose.ui.theme.CustomTextFieldComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +28,20 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        var text by remember { mutableStateOf("") }
 
+                        OutlinedPasswordTextField(
+                            value = text,
+                            onValueChange = { newValue ->
+                                text = newValue
+                            }
+                        )
+                    }
                 }
             }
         }
---
+++ custom-text-field-compose/src/main/java/com/roman/custom_text_field_compose/OutlinedPasswordTextField.kt
@@ -0,0 +1,92 @@
+package com.roman.custom_text_field_compose
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OutlinedPasswordTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit) = { Text(text = stringResource(R.string.password)) },
+    placeholder: @Composable (() -> Unit) = { Text(text = stringResource(R.string.enter_password)) },
+    isError: Boolean = false,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = MaterialTheme.shapes.small,
+    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors()
+) {
+    var passwordIsVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = { newValue ->
+            onValueChange(newValue)
+        },
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = textStyle,
+        label = label,
+        placeholder = placeholder,
+        trailingIcon = {
+            val icon = if (passwordIsVisible) {
+                R.drawable.baseline_visibility_24
+            } else {
+                R.drawable.baseline_visibility_off_24
+            }
+            IconButton(
+                onClick = {
+                    passwordIsVisible = !passwordIsVisible
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = null
+                )
+            }
+        },
+        isError = isError,
+        singleLine = true,
+        keyboardActions = keyboardActions,
+        visualTransformation = if (passwordIsVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password
+        ),
+        interactionSource = interactionSource,
+        shape = shape,
+        colors = colors
+    )
+}
